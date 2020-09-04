e6c74620acbe72d5f12966a9edf39936410ded4b
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-04 20:08:32 +0530

    Adding Variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main (String[] args){
         System.out.println("Hello Meri Jann");
+        int myFirstNumber=35;
+        System.out.println(myFirstNumber);
     }
 }
