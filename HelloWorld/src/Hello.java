b7a8422a6be586056e3a00d3514157ace587f137
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-04 21:21:55 +0530

    Using Expressions

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,6 +2,11 @@ public class Hello {
     public static void main (String[] args){
         System.out.println("Hello Meri Jann");
         int myFirstNumber=35;
-        System.out.println(myFirstNumber);
+        int mySecondNumber=78;
+        int myThirdNumber=90;
+        int myTotal=myFirstNumber+mySecondNumber+myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne=1000-myTotal;
+        System.out.println(myLastOne);
     }
 }
