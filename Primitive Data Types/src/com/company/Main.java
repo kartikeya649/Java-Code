c342ce52338a0ab44947c72aa14208d3367c0a54
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-04 21:59:50 +0530

    Adding two more types

--- Primitive Data Types/src/com/company/Main.java
+++ Primitive Data Types/src/com/company/Main.java
@@ -10,5 +10,29 @@ public class Main {
         System.out.println("Integer Maximum Value:"+ myMaxValue);
         System.out.println("Busted Minimum Value:"+ (myMinValue+1));
         System.out.println("Busted Maximum Value:"+ (myMaxValue-1));
+        
+        int myMaxIntTest=2_147_483_647;
+
+        byte myMinByteValue=Byte.MIN_VALUE;
+        byte myMaxByteValue=Byte.MAX_VALUE;
+            System.out.println("Byte Minimum Value:"+ myMinByteValue);
+            System.out.println("Byte Maximum Value:"+ myMaxByteValue);
+
+        short myMinShortValue=Short.MIN_VALUE;
+        short myMaxShortValue=Short.MAX_VALUE;
+            System.out.println("Short Minimum Value:"+ myMinShortValue);
+            System.out.println("Short Maximum Value:"+ myMaxShortValue);
+
+
+        long myLongValue=1000;
+        long myMinLongValue=Long.MIN_VALUE;
+        long myMaxLongValue=Long.MAX_VALUE;
+            System.out.println("long Minimum Value:"+ myMinLongValue);
+            System.out.println("long Maximum Value:"+ myMaxLongValue);
+
+        long bigLongLiteralValue=2_147_483_647_2434L;
+        System.out.println(bigLongLiteralValue);
+        
+        
     }
 }
