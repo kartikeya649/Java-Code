3a6489b985ef47c557c88701190e97288bdb8a0d
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-05 17:34:23 +0530

    Adding Float & Double

---
+++ Float & Doubles/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float myMinFloatValue=Float.MIN_VALUE;
+        float myMaxFloatValue=Float.MAX_VALUE;
+        System.out.println("Float Minimum No.:"+ myMinFloatValue);
+        System.out.println("Float Maximum No.:"+ myMaxFloatValue);
+
+        double myMinDoubleValue=Double.MIN_VALUE;
+        double myMaxDoubleValue=Double.MAX_VALUE;
+        System.out.println("Double Minimum No.:"+ myMinDoubleValue);
+        System.out.println("Double Maximum No.:"+ myMaxDoubleValue);
+
+        int myIntValue=9;
+        float myFloatValue=8.9f;
+        double myDoubleValue=20.09d;
+    }
+}
