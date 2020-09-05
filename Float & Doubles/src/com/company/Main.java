d10a9b42f680e4b596331d276cf29fbdca054007
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-05 17:49:15 +0530

    Adding more types

--- Float & Doubles/src/com/company/Main.java
+++ Float & Doubles/src/com/company/Main.java
@@ -14,8 +14,25 @@ public class Main {
         System.out.println("Double Minimum No.:"+ myMinDoubleValue);
         System.out.println("Double Maximum No.:"+ myMaxDoubleValue);
 
-        int myIntValue=9;
-        float myFloatValue=8.9f;
-        double myDoubleValue=20.09d;
+        int myIntValue=9/2;
+        float myFloatValue=8.9f/3f;
+        double myDoubleValue=20.09/3;
+        System.out.println("MyIntValue:"+ myIntValue);
+        System.out.println("MyFloatValue:" + myFloatValue);
+        System.out.println("MyDoubleValue:"+ myDoubleValue);
+
+        //Converting pound to KG
+
+        double numberOfPounds=2000;
+        double conertedkg=numberOfPounds*0.45359237;
+        System.out.println("Converted Kilograms:" + conertedkg);
+
+        //pi values
+
+        double pi=3.1415927;
+        double anotherNumber=3_000_000.4_567_890;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+
     }
 }
