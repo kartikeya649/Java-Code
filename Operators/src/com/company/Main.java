a1ccae8a2188b1fb356a28035deb8849ff69499b
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-06 08:42:06 +0530

    Updating Operators

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -22,5 +22,23 @@ public class Main {
         result%=3;
         System.out.println("4%3:"+result);
 
+        result++;
+        System.out.println("1+1:"+result);
+
+        result--;
+        System.out.println("2-1:"+result);
+
+        result+=2;
+        System.out.println("1+2:"+result);
+
+        result*=10;
+        System.out.println("3*10:"+result);
+
+        result/=3;
+        System.out.println("30/3:"+result);
+
+        result-=2;
+        System.out.println("10-2:"+result);
+
     }
 }
