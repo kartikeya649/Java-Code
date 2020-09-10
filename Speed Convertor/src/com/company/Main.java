89f54acfbcff6afe49622f64632974e966a2ab61
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-10 17:54:20 +0530

    Speed Convertor
    Challenge 1

---
+++ Speed Convertor/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double miles=SpeedConvertor.toMilesPerHour(10.5);
+        System.out.println("Miles= "+miles);
+        SpeedConvertor.printConversion(10.5);
+	// write your code here
+    }
+}
