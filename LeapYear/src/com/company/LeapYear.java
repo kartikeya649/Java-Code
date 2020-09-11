3285b0299bdac2123e43473bfd01147c0d6c0c19
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-11 18:01:00 +0530

    Leam year

---
+++ LeapYear/src/com/company/LeapYear.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class LeapYear {
+    public static boolean isLeapYear(int year){
+        if(year>=1 && year<=9999){
+            if((year%4==0 && year%100!=0) || (year%400==0)){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    }
+
