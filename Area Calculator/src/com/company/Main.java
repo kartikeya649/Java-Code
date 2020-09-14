f95f5f3e2a5ecad94593927559d9fc98ea38b434
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-14 08:54:09 +0530

    Area Calculator

---
+++ Area Calculator/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(AreaCalculator.area(56));
+        System.out.println(AreaCalculator.area(78,89));
+    }
+}
