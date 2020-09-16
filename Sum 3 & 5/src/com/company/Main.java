9501b8485f794b601af67c41d4b45d36e04337c2
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-16 17:46:48 +0530

    Sum of 3 & 5 chllng

---
+++ Sum 3 & 5/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int count=0;
+        int sum=0;
+        for (int i=1;i<=1000;i++){
+            if ((i%3==0)&&(i%5==0)){
+                count++;
+                sum+=i;
+                System.out.println("Found Number:"+i);
+            }
+            if (count==5){
+                break;
+            }
+        }
+        System.out.println("Sum:"+sum);
+    }
+}
