ccd21c59b390a9416a928cac784e404da1674feb
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-22 17:57:42 +0530

    Adding challenge

--- Reading User Input/src/com/company/Main.java
+++ Reading User Input/src/com/company/Main.java
@@ -27,6 +27,32 @@ public class Main {
             System.out.println("Unable to parse year of Birth");
         }
 
+
+        System.out.println("####################Challenge########################");
+
+
+        int counter=0;
+        int sum=0;
+
+        //while (true){
+        while (counter<10){
+            int order=counter+1;
+            System.out.println("Enter Number # "+order+":");
+
+            boolean isAnInt=scanner.hasNextInt();
+            if (isAnInt){
+                int number=scanner.nextInt();
+                counter++;
+                sum+=number;
+                /*if (counter==10){
+                    break;
+                }*/
+            }else {
+                System.out.println("Invalid Number");
+            }
+            scanner.nextLine();//Handle end of the line
+        }
+        System.out.println("sum: "+sum);
         scanner.close();
     }
 }
