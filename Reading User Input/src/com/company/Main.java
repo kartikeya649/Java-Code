3441a52b68d131c89309ed070ca310fafe3c1506
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-22 17:37:23 +0530

    Reading User Update

--- Reading User Input/src/com/company/Main.java
+++ Reading User Input/src/com/company/Main.java
@@ -8,14 +8,25 @@ public class Main {
 	// write your code here
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth=scanner.nextInt();
-        scanner.nextLine();//Handle Next Line Character
 
-        System.out.println("Enter your Name: ");
-        String name=scanner.nextLine();
-        int age=2020-yearOfBirth;
+        boolean hasNextInt=scanner.hasNextInt();
+        if(hasNextInt){
+            int yearOfBirth=scanner.nextInt();
+            scanner.nextLine();//Handle Next Line Character
 
-        System.out.println("Your name is "+name+", and you are "+age+" years old.");
+            System.out.println("Enter your Name: ");
+            String name=scanner.nextLine();
+            int age=2020-yearOfBirth;
+
+            if(age>=0 && age<=150) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            }else {
+                System.out.println("Invalid Year of Birth");
+            }
+        }else {
+            System.out.println("Unable to parse year of Birth");
+        }
+
         scanner.close();
     }
 }
