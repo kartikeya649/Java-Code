70c19deb37fa44920426992eaf0d6992d682e3d7
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-28 17:48:43 +0530

    Inheritence Part 1

---
+++ Inheritence/src/com/company/Dog.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Dog extends Animal {
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(int eyes, int legs, int tail, int teeth, String coat,String name,int size,int weight) {
+        super(name,1,1,size,weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+    private void chew(){
+        System.out.println("Dog.chew() is called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat();
+    }
+}
---
+++ Inheritence/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Animal animal=new Animal("Bista",1,1,20,290);
+        Dog dog=new Dog(2,4,1,25,"Brown","Allie",20,20);
+        dog.eat();
+    }
+}
