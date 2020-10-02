de7375800b4e79345ab18c0b48925a42f3780d99
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-10-02 17:16:52 +0530

    Inheritance Challenge
    End of Section 6

---
+++ Inheritance Challange/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        OutLander outLander=new OutLander(90);
+        outLander.accelerate(87);
+        outLander.steer(76);
+        outLander.accelerate(-98);
+    }
+}
---
+++ Inheritance Challange/src/com/company/OutLander.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class OutLander extends Car{
+    private int roadServiceMonths;
+
+    public OutLander(int roadServiceMonths){
+        super("Outlander","4WD",5,5,6,false);
+        this.roadServiceMonths=roadServiceMonths;
+    }
+    public void accelerate(int rate){
+        int newVelocity=getCurrentVelocity()+rate;
+        if (newVelocity==0){
+            stop();
+        }else if (newVelocity>0 && newVelocity<=10){
+            changeGear(1);
+        }else if (newVelocity>10 && newVelocity <=20){
+            changeGear(2);
+        }else if (newVelocity>20 && newVelocity <=30){
+            changeGear(3);
+        }else {
+            changeGear(4);
+        }
+        if(newVelocity>0){
+            changeVelocity(newVelocity,getCurrentDirection());
+        }
+    }
+}
