df0ae9c6f903b792a4118ebb5687a4fde3a85f7a
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-10-20 20:59:18 +0530

    Encapsulation P1

---
+++ Encapsulation/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /*Player player=new Player();
+        player.name="Kartik";
+        //player.health=20;
+        player.weapon="Sword";
+
+        int damage=10;
+        player.loseHealth(damage);
+        System.out.println("Remaining Health: "+player.healthRemaining());
+
+        damage=11;
+        player.health=200;
+        player.loseHealth(damage);
+        System.out.println("Remaining Health: "+player.healthRemaining());*/
+        EnhancedPlayer player=new EnhancedPlayer("Kartik",89,"Sword");
+        System.out.println("Initial health is: "+player.getHealth());
+        System.out.println("################################Challange######################################");
+    }
+}
