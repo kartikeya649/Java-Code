54c69752d5e9c61a3df61251fc95e7a68a2a896b
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-11-24 16:17:56 +0530

    Fixing bug

--- ArrayList Challenge/src/com/company/MobilePhone.java
+++ ArrayList Challenge/src/com/company/MobilePhone.java
@@ -25,6 +25,10 @@ public class MobilePhone {
         if (foundPosition<0){
             System.out.println(oldContact.getName()+", was't found");
             return false;
+        } else if(findContact(newContact.getName()) != -1) {
+            System.out.println("Contact with name " + newContact.getName() +
+                " already exists.  Update was not successful.");
+            return false;
         }
         this.myContacts.set(foundPosition,newContact);
         System.out.println(oldContact.getName()+", was replaced with "+newContact.getName());
