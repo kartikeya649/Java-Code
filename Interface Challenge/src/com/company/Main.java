35fb1c67fc80ba9e6fefa5daa35442e0c01c94ff
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-12-23 22:53:28 +0530

    Interface Challenge

---
+++ Interface Challenge/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static ArrayList<String> readValues(){
+        ArrayList<String> values=new ArrayList<String>();
+        Scanner scanner=new Scanner(System.in);
+        boolean Quit =false;
+        int index=0;
+        System.out.println("Choose\n"+"1 to enter a string\n"+"o to quit");
+
+        while (!Quit){
+            System.out.println("Choose an option");
+            int choice=scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 0:
+                    Quit=true;
+                    break;
+                case 1:
+                    System.out.println("Enter a string: ");
+                    String stringInput=scanner.nextLine();
+                    values.add(index,stringInput);
+                    index++;
+                    break;
+            }
+        }
+    }
+}
